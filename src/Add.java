9e9b908d352b969ca9bb8e108de14b3f2f6aa563
zrashid007 <95605403+zrashid007@users.noreply.github.com>
2022-05-11 22:59:44 +0600

    Changing variable name sum to sumetion

--- src/Add.java
+++ src/Add.java
@@ -2,9 +2,9 @@
 public class Add
 {
     public static void main(String args[])
-    {
+    {   sumetion
         int number1 = 225, number2 = 115, sum;
-        sum = number1 + number2;
+        sumetion = number1 + number2;
         System.out.println("The sum of numbers is: "+sum);
     }
 }
