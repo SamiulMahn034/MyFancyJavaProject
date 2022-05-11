c24a36cfbada803664065c8834c33d0f39651b32
SamiulMahn034 <mmahin181034@bscse.uiu.ac.bd>
2022-05-11 23:09:55 +0600

    Substraction method added

--- src/Add.java
+++ src/Add.java
@@ -8,3 +8,13 @@ public class Add
         System.out.println("The sum of numbers is: "+sum);
     }
 }
+
+public class Sub
+{
+    public static void main(String args[])
+    {
+        int number1 = 225, number2 = 115, sum;
+        Sub = number1 - number2;
+        System.out.println("The sum of numbers is: "+Sub);
+    }
+}
